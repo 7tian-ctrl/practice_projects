4429970e93db1ecd7d9e343d0008e7d3e8306b34
7tian-ctrl <visheshuniyal34@gmail.com>
2026-04-10 06:42:45 +0200

    initial upload

---
+++ main.c
@@ -0,0 +1,561 @@
+//SEQUENCE OF PROGRAM
+//
+//Libraries used	-	{stdio, stdlib, string, ctype, inttypes}
+//Defined types and structures		-	{unsigned 64bit int, unsigned 32bit int, string, contact, database}
+//
+//FUNCTION PROTOTYPES
+//
+//Global Variable 	-	{database db}
+//Database Handling Functions	-	 {new_string, string_compare, create_contact, insert_contact, remove_contact}
+//Sorting Functions	-	{merge, sort_merge, sort_db}
+//Searching Functions	-	{search_by_name, search_by_phone}
+//Display Functions	-	{display, display_contact, display_menu, exit_display}
+//User Input Fuctions	-	{phone_input, string_input, contact_input}
+//Safety Functions	-	{free_database}
+//Load Off Functions	-	{boiler_work, contact_insertion_work, ask_contact_info, removal_of_contact, phone_search, name_search}
+
+//MAIN Function		-	{main}
+
+//TO DO LIST
+//
+//search_by_phone	-	completed
+//contact_input		-	completed	--update to check for existing names	completed
+//contact_removal	-	completed
+//menu			-	completed
+//read from file	-			--different file support
+//write to file
+//multi user files
+//authentication
+//fix string_input
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <inttypes.h>		
+
+//------------------------------------
+//defined types and structures
+
+typedef uint64_t u64;
+typedef uint32_t u32;
+
+typedef struct {
+        char* data;
+        u32 length;
+} string;
+
+typedef struct {
+        string name;
+        u64 phone;
+} contact;
+
+typedef struct {
+        contact** arr;
+	u32 capacity;
+        u32 top;
+} database;
+
+//GLOBAL VARIABLE
+
+database db = { .arr = NULL, .top = 0, .capacity = 0 };
+
+//----------------------------------------------------------
+
+
+//----------------------------------------------------------
+// FUNCTION PROTOTYPES
+//----------------------------------------------------------
+
+// String & Memory Utilities
+string new_string(char* data);
+int string_compare(string a, string b);
+void free_database(database* db);
+
+// Database Core Operations
+contact* create_contact(string name, u64 ph);
+u32 insert_contact(database* x, contact* n);
+void contact_removal(database* x, string n);
+
+// Sorting (Merge Sort)
+void merge(contact** arr, u32 left, u32 mid, u32 right);
+void sort_merge(contact** arr, u32 left, u32 right);
+void sort_db(database* x);
+
+// Searching
+contact* search_by_name(database* x, string target);
+contact* search_by_phone(database* x, u64 ph);
+
+// Display & UI
+void display_db(database* x);
+void display_contact(contact* c);
+void display_menu();
+void exit_display(database* db);
+
+// User Input Wrappers
+u64 phone_input();
+string string_input();
+contact* contact_input(database* db);
+
+// High-Level Logic (Load Off)
+void boiler_work(database* db, string s, u64 p);
+void contact_insertion_work(database* db, contact* c);
+void ask_contact_info(database* db);
+void removal_of_contact(database* x);
+void name_search(database* x);
+void phone_search(database* x);
+
+//----------------------------------------------------------
+
+// MAIN FUNCTION
+
+void main() {	
+
+	u32 selection = 0;
+
+	while(selection != 6) {
+		display_menu();
+
+		scanf("%u", &selection);
+
+		if (selection == 1) {
+			ask_contact_info(&db);
+		}
+
+		if (selection == 2) {
+			display_db(&db);
+		}
+
+		if (selection == 3) {
+			name_search(&db);
+		}
+
+		if (selection == 4) {
+			phone_search(&db);
+		}
+
+		if (selection == 5) {
+			removal_of_contact(&db);
+		}
+
+		if (selection == 6) {
+			exit_display(&db);
+		}
+
+		while (getchar() != '\n');
+	}
+}
+
+
+//----------------------------------------------------------
+//DATABASE HANDLING FUNCTIONS
+
+string new_string(char* data) { 
+	return (string) { .data = data, .length = strlen(data) }; 
+}
+
+int string_compare(string a, string b) {
+	u32 i = 0;
+
+	if (a.length > b.length) return 1;
+	if (a.length < b.length) return -1;
+
+	for (i; i < a.length; i++) {
+		if (a.data[i] == b.data[i]) {
+			continue;
+		}
+
+		if (a.data[i] > b.data[i]) return 1;
+		if (a.data[i] < b.data[i]) return -1;
+	}
+
+	return 0;
+}
+
+contact* create_contact(string name, u64 ph) {
+        contact* new = (contact*)malloc(sizeof(contact));
+
+        if(new == NULL) return NULL;
+
+	if (name.data != NULL && name.length > 0) {
+		name.data[0] = (char)toupper((unsigned char)name.data[0]);
+	}
+
+        new->name = name;
+        new->phone = ph;
+
+        return new;
+}
+
+u32 insert_contact(database* x, contact* n) {
+        if(x->top >= x->capacity) {
+                u32 new_capacity = (x->capacity == 0) ? 4 : x->capacity * 2;
+
+                contact** temp = realloc(x->arr, sizeof(contact*) * new_capacity);
+
+                if(temp == NULL) return 1;
+
+                x->arr = temp;
+                x->capacity = new_capacity;
+        }
+
+        x->arr[x->top++] = n;
+
+        return 0;
+}
+
+void contact_removal(database* x, string n) {
+	if (x == NULL || x->top == 0) {
+		printf("Database is empty.\n");
+		return;
+	}
+
+	if (n.data == NULL || n.length == 0) {
+		printf("\n\t\tEmpty Input.\n");
+		return;
+	}
+	
+	u32 low = 0;
+	u32 high = x->top - 1;
+	int found_index = -1;
+
+	while (low <= high) {
+		u32 mid = low + (high - low) / 2;
+		int res = string_compare(x->arr[mid]->name, n);
+
+		if (res == 0) {
+			found_index = mid;
+			break;
+		}
+		if (res < 0) low = mid + 1;
+		
+		else {
+			if (mid == 0) break;
+			high = mid - 1;
+		}
+	}
+
+	if (found_index == -1) {
+		printf("Contact '%s' not found.\n", n.data);
+		return;
+	}
+
+	free(x->arr[found_index]->name.data);
+	free(x->arr[found_index]);
+
+	for (u32 i = found_index; i < x->top - 1; i++) {
+		x->arr[i] = x->arr[i + 1];
+	}
+
+	x->top--;
+	printf("\n\t\tContact '%s' removed.\n", n.data);
+}
+
+//--------------------------------------------------------------------------
+
+//-------------------------------------------------------------
+//SORTING FUNCTIONS
+
+void merge(contact** arr, u32 left, u32 mid, u32 right) {
+	u32 n1 = mid - left + 1;
+	u32 n2 = right - mid;
+
+	contact** L = malloc(n1 * sizeof(contact *));
+	contact** R = malloc(n2 * sizeof(contact *));
+
+	for (u32 i = 0; i < n1; i++) L[i] = arr[left + i];
+	for (u32 j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
+
+	u32 i = 0, j = 0, k = left;
+
+
+	while (i < n1 && j < n2) {
+		if (string_compare(L[i]->name, R[j]->name) <= 0) {
+		arr[k++] = L[i++];
+		} else {
+			arr[k++] = R[j++];
+		}
+	}
+
+
+	while (i < n1) arr[k++] = L[i++];
+	while (j < n2) arr[k++] = R[j++];
+
+	free(L);
+	free(R);
+}
+
+void sort_merge(contact** arr, u32 left, u32 right) {
+	if (left < right) {
+        u32 mid = left + (right - left) / 2;
+
+        sort_merge(arr, left, mid);      
+        sort_merge(arr, mid + 1, right); 
+
+        merge(arr, left, mid, right);    
+    }
+}
+
+void sort_db(database* x) {
+    if (x == NULL || x->top < 2) return;
+    sort_merge(x->arr, 0, x->top - 1);
+}
+
+//--------------------------------------------------------------
+
+//-------------------------------------------------------------
+//SEARCHING FUNCTIONS
+
+contact* search_by_name(database* x, string target) {
+	if (x == NULL || x->arr == NULL || x->top == 0) return NULL;
+
+	u32 low = 0;
+	u32 high = x->top - 1;
+
+	while (low <= high) {
+		u32 mid = low + (high - low) / 2; 
+
+		int res = string_compare(x->arr[mid]->name, target);
+
+		if (res == 0) {
+			return x->arr[mid];
+		}
+
+		if (res < 0) {
+			low = mid + 1;
+		} else {
+			if (mid == 0) break;
+			high = mid - 1;
+		}
+	}
+
+	return NULL;
+}
+
+contact* search_by_phone(database* x, u64 ph) {
+	if (x == NULL || x->top == 0) return NULL;
+
+	for (u32 i = 0; i < x->top; i++) {
+		if (x->arr[i]->phone == ph) {
+			return x->arr[i];
+		}
+	}
+
+	return NULL;	
+}
+
+//---------------------------------------------------------------
+
+//----------------------------------------------------------
+//DISPLAY FUNCTIONS
+
+void display_db(database* x) {
+
+	if (x == NULL || x->top == 0) {
+		printf("Database is empty.\n");
+		return;
+ 	}
+
+	printf("\n--- Contact List (%u contacts) ---\n", x->top);
+	printf("%-20s | %-15s\n", "Name", "Phone");
+	printf("------------------------------------------\n");
+
+	for (u32 i = 0; i < x->top; i++) {
+		
+		contact* c = x->arr[i];
+		
+		printf("%-20s | %llu\n", c->name.data, (unsigned long long)c->phone);
+	}
+	
+	printf("------------------------------------------\n");
+}
+
+void display_contact(contact* c) {
+	if(c == NULL) {
+		printf("<<<<<<<<<<<<<<<<<              Does not exist.              >>>>>>>>>>>>>>>>>");
+		return;
+	}
+	
+
+	printf("\n\n%-20s | %-15s\n", "Name", "Phone");
+	printf("------------------------------------------\n");		
+
+	printf("%-20s | %llu\n\n", c->name.data, (unsigned long long)c->phone);
+}
+
+void display_menu() {
+	printf("\n\033[1;36m==========================================\033[0m\n");
+	printf("\033[1;34m        CONTACT MANAGEMENT SYSTEM         \033[0m\n");
+	printf("\033[1;36m==========================================\033[0m\n");
+	printf("  [1] \033[32mAdd New Contact\033[0m\n");
+	printf("  [2] \033[32mDisplay All Contacts\033[0m\n");
+	printf("  [3] \033[32mSearch by Name\033[0m\n");
+	printf("  [4] \033[32mSearch by Phone\033[0m\n");
+	printf("  [5] \033[31mDelete Contact\033[0m\n");
+	printf("  [6] \033[33mExit Program\033[0m\n");
+	printf("\033[1;36m------------------------------------------\033[0m\n");
+	printf("Selection > ");
+}
+
+
+void exit_display(database* db) {
+	printf("\033[H\033[J"); 
+
+	printf("\n\033[1;33m[!] Shutting down Contact Management System...\033[0m\n");
+    
+
+	printf("\033[0;90m  > Freeing database memory...\033[0m\n");
+	free_database(db);
+    
+	printf("\033[0;90m  > Closing buffers...\033[0m\n");
+    
+
+	printf("\n\033[1;32mDone. Goodbye!\033[0m\n");
+	printf("\033[1;36m------------------------------------------\033[0m\n\n");
+    
+	exit(0);
+}
+
+//------------------------------------------------------------------------------
+
+//--------------------------------------------------------------------
+//USER INPUT FUNCTIONS
+
+u64 phone_input() {
+	u64 phone;
+	printf("\033[1;32mEnter Phone Number:\033[0m "); 
+
+	if (scanf("%" SCNu64, &phone) != 1) {
+		printf("\033[1;31mInvalid input! Please enter digits only.\033[0m\n");
+		while(getchar() != '\n'); 
+	}
+
+	return phone;
+}
+
+string string_input() {
+	u32 cap = 31;
+	char* n = (char*)malloc(sizeof(char) * cap);
+
+	string empty_string = { .data = NULL, .length = 0 };
+
+	if (n == NULL) return empty_string;
+
+	printf("Please enter the name: ");
+	if(scanf(" %30[^\n]s", n) != 1) {
+		return empty_string;
+	}
+
+	if (n != NULL && strlen(n) > 0) {
+		n[0] = (char)toupper((unsigned char)n[0]);
+	}
+
+	string name = new_string(n);
+	
+	return name;
+}
+
+contact* contact_input(database* db) {
+	u64 ph;
+	u32 cap = 31;
+	char* n = (char*)malloc(sizeof(char) * cap);
+
+	if (n == NULL) return NULL;
+
+	printf("Please enter the name: ");
+		if(scanf(" %30[^\n]s", n) != 1) {
+		free(n);
+		return NULL;
+	}
+
+	printf("Please enter the phone number: ");
+		if(scanf("%" SCNu64, &ph) != 1) {
+			free(n);
+			return NULL;
+	}
+
+	if (n != NULL && strlen(n) > 0) {
+		n[0] = (char)toupper((unsigned char)n[0]);
+	}
+
+	string name = new_string(n);
+
+	if (search_by_name(db, name) != NULL) {
+		printf("\n\t\tName already exists!\t\tTry again.\n");
+		free(n);
+		return NULL;
+	}
+
+	return create_contact(name, ph); 
+}
+
+//-------------------------------------------------------------------
+//SAFETY FUNCTIONS
+
+void free_database(database* db) {
+	if (db == NULL || db->arr == NULL) return;
+
+	for (u32 i = 0; i < db->top; i++) {
+		if (db->arr[i]->name.data != NULL) {
+			free(db->arr[i]->name.data);
+		}
+		free(db->arr[i]);
+	}
+
+	free(db->arr);    
+
+	db->arr = NULL;
+	db->top = 0;
+	db->capacity = 0;
+}
+
+//-------------------------------------------------------------------
+
+//------------------------------------------------------
+//LOAD OFF FUNCTIONS
+
+void boiler_work(database* db, string s, u64 p) {
+	contact* a = create_contact(s, p);
+	insert_contact(db, a);
+	
+	return;
+}
+
+void contact_insertion_work(database* db, contact* c) {
+	insert_contact(db, c);
+	return;
+}
+
+void ask_contact_info(database* db) {
+	contact* c = contact_input(db);
+	if (c == NULL) return;
+
+	contact_insertion_work(db, c);
+
+	sort_db(db);
+	display_db(db);
+
+	return;
+}
+
+void removal_of_contact(database* x) {
+	string s = string_input();
+	contact_removal(x, s);
+	display_db(x);
+}
+
+void name_search(database* x) {
+	string s = string_input();
+	contact* c = search_by_name(x, s);
+
+	display_contact(c);
+}
+
+void phone_search(database* x) {
+	contact* c = search_by_phone(x, phone_input());
+
+	display_contact(c);
+}
+
+//----------------------------------------------------
+
